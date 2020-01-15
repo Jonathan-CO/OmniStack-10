b728321b25671af7c1eaa103dccd374f518af9be
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-01-14 21:55:30 -0300

    cria model Dev e pega user com a API do github

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.post('/users', (req, res)=>{ 
-    console.log(req.body);
+routes.post('/devs', async (req, res)=>{ 
     // return res.send("Hello Word"); //send envia um texto
+    const {github_username} = req.body;
+    const response = await axios.get(`https://api.github.com/users/${github_username}`);
+    console.log(response.data);
     return res.json({ message: "Hello Jonathan Helenium"}); //send envia um json
 });
 
