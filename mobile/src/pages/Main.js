da7a5477103e7294bd6ce2d65ea2aefb6b19a373
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-01-19 21:49:37 -0300

    finaliza form com MaterialIcons

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import MapView, {Marker, Callout} from 'react-native-maps';
-import {StyleSheet, Image, View, Text} from 'react-native';
+import {StyleSheet, Image, View, Text, TextInput, TouchableOpacity} from 'react-native';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
+import {MaterialIcons} from '@expo/vector-icons';
 
 
 function Main({navigation}){
@@ -33,20 +34,34 @@ function Main({navigation}){
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-           <Marker coordinate={{latitude: -18.2375635, longitude: -43.6139873}}>
-               <Image style={styles.image} source={{uri: 'https://avatars3.githubusercontent.com/u/31313701?s=460&v=4'}} />
-               <Callout onPress={()=>{
-                   navigation.navigate('Profile', {github_username: 'Jonathan-CO'});
-               }}>
-                   <View style={styles.callout}>
-                       <Text style={styles.devName}>Jonathan da Cunha</Text>
-                       <Text style={styles.devBio}>Bio de jonathan da Cunha Oliveira</Text>
-                       <Text style={styles.devTechs}> ReacJS, React Native, NodeJS</Text>
-                   </View>
-               </Callout>
-           </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{latitude: -18.2375635, longitude: -43.6139873}}>
+                <Image style={styles.image} source={{uri: 'https://avatars3.githubusercontent.com/u/31313701?s=460&v=4'}} />
+                <Callout onPress={()=>{
+                    navigation.navigate('Profile', {github_username: 'Jonathan-CO'});
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Jonathan da Cunha</Text>
+                        <Text style={styles.devBio}>Bio de jonathan da Cunha Oliveira</Text>
+                        <Text style={styles.devTechs}> ReacJS, React Native, NodeJS</Text>
+                    </View>
+                </Callout>
+            </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs"
+                    placeholderTextColor="#999"
+                    autoCapitalize= "words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity onPress={()=>{}} style={styles.loadButton}>
+                     <MaterialIcons name="my-location" size={20} color ="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
@@ -74,6 +89,40 @@ const styles = StyleSheet.create({
     },
     devTechs:{
         marginTop: 5
+    },
+    searchForm:{
+        position: 'absolute',
+        top:20,
+        left: 20,
+        right:20,
+        zIndex:5,
+        flexDirection: 'row',
+    },
+    searchInput:{
+        flex:1,
+        height: 50,
+        backgroundColor: "#FFF",
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+    loadButton:{
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+
     }
 })
 
